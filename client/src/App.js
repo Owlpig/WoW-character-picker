73e0df80a8ccca106ec246da04f085ab5bc7101c
Owlpig <henry.mispel@gmail.com>
2021-03-10 16:21:02 +0100

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [apiResponse, setApiResponse] = useState({ res: '' });
+
+  useEffect(() => {
+    fetch('http://localhost:3003/testAPI')
+      .then(res => res.text())
+      .then(res => setApiResponse({ res }));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{apiResponse.res}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
