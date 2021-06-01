106a2bf6fc3db6a21c5e051a2ac4a2b8aa0680e1
Owlpig <henry.mispel@gmail.com>
2021-06-01 11:41:55 +0200

    more backend stuff

---
+++ server/db/armor.js
@@ -0,0 +1,16 @@
+const armor = [
+  { id: 1, type: 'Plate' },
+  { id: 2, type: 'Plate' },
+  { id: 3, type: 'Mail' },
+  { id: 4, type: 'Leather' },
+  { id: 5, type: 'Cloth' },
+  { id: 6, type: 'Plate' },
+  { id: 7, type: 'Mail' },
+  { id: 8, type: 'Cloth' },
+  { id: 9, type: 'Cloth' },
+  { id: 10, type: 'Leather' },
+  { id: 11, type: 'Leather' },
+  { id: 12, type: 'Leather' },
+];
+
+module.exports = armor;
--- server/routes/classes.js
+++ server/routes/classes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const Classes = require('../models/classModel');
+const armor = require('../db/armor');
 
 const router = express.Router();
 const accessToken = process.env.API_TOKEN;
@@ -17,6 +18,18 @@ const fetchSpecs = async id => {
   return specData;
 };
 
+const fetchSpecDetails = async id => {
+  const rawSpecs = await fetch(`https://us.api.blizzard.com/data/wow/playable-specialization/${id}?namespace=static-us&locale=en_US&access_token=${accessToken}`);
+  const specData = rawSpecs.json();
+  return specData;
+};
+
+const fetchSpecMedia = async id => {
+  const rawSpecs = await fetch(`https://us.api.blizzard.com/data/wow/media/playable-specialization/${id}?namespace=static-us&locale=en_US&access_token=${accessToken}`);
+  const specData = rawSpecs.json();
+  return specData;
+};
+
 router.get('/', async (req, res) => {
   const classes = await fetchClasses();
   res.send(classes);
@@ -28,6 +41,48 @@ router.put('/', async (req, res) => {
     const { specializations } = await fetchSpecs(c.classId);
     const dbSpecs = c.specs;
     specializations.forEach(s => dbSpecs.push({ name: s.name, id: s.id }));
+    c.save();
+  });
+
+  res.send(dbClasses);
+});
+
+router.put('/armor', async (req, res) => {
+  const dbClasses = await Classes.find();
+  dbClasses.forEach(async c => {
+    const dbArmor = c;
+    const armorType = armor.find(a => a.id === c.classId);
+    dbArmor.armor = armorType.type;
+    c.save();
+  });
+
+  res.send(dbClasses);
+});
+
+router.put('/roles', async (req, res) => {
+  const dbClasses = await Classes.find();
+  dbClasses.forEach(async c => {
+    c.specs.forEach(async s => {
+      const specDetails = await fetchSpecDetails(s.id);
+      const spec = s;
+      spec.role = specDetails.role.name;
+      s.save({ suppressWarning: true });
+    });
+    c.save();
+  });
+
+  res.send(dbClasses);
+});
+
+router.put('/media', async (req, res) => {
+  const dbClasses = await Classes.find();
+  dbClasses.forEach(async c => {
+    c.specs.forEach(async s => {
+      const specDetails = await fetchSpecMedia(s.id);
+      const spec = s;
+      spec.icon = specDetails.assets[0].value;
+      s.save({ suppressWarning: true });
+    });
     c.save();
   });
 
